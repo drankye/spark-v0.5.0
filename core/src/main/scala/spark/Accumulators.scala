3582e9a5a1ff8547bf322573f7a48aa8c8561393
Kai Zheng <zhengkai.zk@alibaba-inc.com>
2018-01-06 13:36:39 +0800

    Initially import spark 0.5.0 source codes

---
+++ core/src/main/scala/spark/Accumulators.scala
@@ -0,0 +1,87 @@
+package spark
+
+import java.io._
+
+import scala.collection.mutable.Map
+
+class Accumulator[T] (
+    @transient initialValue: T,
+    param: AccumulatorParam[T])
+  extends Serializable {
+  
+  val id = Accumulators.newId
+  @transient
+  var value_ = initialValue // Current value on master
+  val zero = param.zero(initialValue)  // Zero value to be passed to workers
+  var deserialized = false
+
+  Accumulators.register(this, true)
+
+  def += (term: T) { value_ = param.addInPlace(value_, term) }
+  def value = this.value_
+  def value_= (t: T) {
+    if (!deserialized) value_ = t
+    else throw new UnsupportedOperationException("Can't use value_= in task")
+  }
+ 
+  // Called by Java when deserializing an object
+  private def readObject(in: ObjectInputStream) {
+    in.defaultReadObject
+    value_ = zero
+    deserialized = true
+    Accumulators.register(this, false)
+  }
+
+  override def toString = value_.toString
+}
+
+trait AccumulatorParam[T] extends Serializable {
+  def addInPlace(t1: T, t2: T): T
+  def zero(initialValue: T): T
+}
+
+// TODO: The multi-thread support in accumulators is kind of lame; check
+// if there's a more intuitive way of doing it right
+private object Accumulators {
+  // TODO: Use soft references? => need to make readObject work properly then
+  val originals = Map[Long, Accumulator[_]]()
+  val localAccums = Map[Thread, Map[Long, Accumulator[_]]]()
+  var lastId: Long = 0
+  
+  def newId: Long = synchronized {
+    lastId += 1
+    return lastId
+  }
+
+  def register(a: Accumulator[_], original: Boolean): Unit = synchronized {
+    if (original) {
+      originals(a.id) = a
+    } else {
+      val accums = localAccums.getOrElseUpdate(Thread.currentThread, Map())
+      accums(a.id) = a
+    }
+  }
+
+  // Clear the local (non-original) accumulators for the current thread
+  def clear: Unit = synchronized { 
+    localAccums.remove(Thread.currentThread)
+  }
+
+  // Get the values of the local accumulators for the current thread (by ID)
+  def values: Map[Long, Any] = synchronized {
+    val ret = Map[Long, Any]()
+    for ((id, accum) <- localAccums.getOrElse(Thread.currentThread, Map())) {
+      ret(id) = accum.value
+    }
+    return ret
+  }
+
+  // Add values to the original accumulators with some given IDs
+  def add(values: Map[Long, Any]): Unit = synchronized {
+    for ((id, value) <- values) {
+      if (originals.contains(id)) {
+        originals(id).asInstanceOf[Accumulator[Any]] += value
+      }
+    }
+  }
+}
---
+++ project/SparkBuild.scala
@@ -0,0 +1,76 @@
+import sbt._
+import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object SparkBuild extends Build {
+  // Hadoop version to build against. For example, "0.20.2", "0.20.205.0", or
+  // "1.0.1" for Apache releases, or "0.20.2-cdh3u3" for Cloudera Hadoop.
+  val HADOOP_VERSION = "0.20.205.0"
+
+  lazy val root = Project("root", file("."), settings = sharedSettings) aggregate(core, repl, examples, bagel)
+
+  lazy val core = Project("core", file("core"), settings = coreSettings)
+
+  lazy val repl = Project("repl", file("repl"), settings = replSettings) dependsOn (core)
+
+  lazy val examples = Project("examples", file("examples"), settings = examplesSettings) dependsOn (core)
+
+  lazy val bagel = Project("bagel", file("bagel"), settings = bagelSettings) dependsOn (core)
+
+  def sharedSettings = Defaults.defaultSettings ++ Seq(
+    organization := "org.spark-project",
+    version := "0.5.0",
+    scalaVersion := "2.9.1",
+    scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
+    unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
+    retrieveManaged := true,
+    transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
+    testListeners <<= target.map(t => Seq(new eu.henkelmann.sbt.JUnitXmlTestsListener(t.getAbsolutePath))),
+    publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
+    libraryDependencies ++= Seq(
+      "org.eclipse.jetty" % "jetty-server" % "7.5.3.v20111011",
+      "org.scalatest" %% "scalatest" % "1.6.1" % "test",
+      "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+    ),
+    /* Workaround for issue #206 (fixed after SBT 0.11.0) */
+    watchTransitiveSources <<= Defaults.inDependencies[Task[Seq[File]]](watchSources.task,
+      const(std.TaskExtra.constant(Nil)), aggregate = true, includeRoot = true) apply { _.join.map(_.flatten) }
+  )
+
+  val slf4jVersion = "1.6.1"
+
+  def coreSettings = sharedSettings ++ Seq(
+    name := "spark-core",
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/",
+      "Cloudera Repository" at "http://repository.cloudera.com/artifactory/cloudera-repos/"
+    ),
+    libraryDependencies ++= Seq(
+      "com.google.guava" % "guava" % "11.0.1",
+      "log4j" % "log4j" % "1.2.16",
+      "org.slf4j" % "slf4j-api" % slf4jVersion,
+      "org.slf4j" % "slf4j-log4j12" % slf4jVersion,
+      "com.ning" % "compress-lzf" % "0.8.4",
+      "org.apache.hadoop" % "hadoop-core" % HADOOP_VERSION,
+      "asm" % "asm-all" % "3.3.1",
+      "com.google.protobuf" % "protobuf-java" % "2.4.1",
+      "de.javakaffee" % "kryo-serializers" % "0.9",
+      "org.jboss.netty" % "netty" % "3.2.6.Final",
+      "it.unimi.dsi" % "fastutil" % "6.4.2"
+    )
+  ) ++ assemblySettings ++ Seq(test in assembly := {})
+
+  def replSettings = sharedSettings ++ Seq(
+    name := "spark-repl",
+    libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _)
+  ) ++ assemblySettings ++ Seq(test in assembly := {})
+
+  def examplesSettings = sharedSettings ++ Seq(
+    name := "spark-examples",
+    libraryDependencies += "colt" % "colt" % "1.2.0"
+  )
+
+  def bagelSettings = sharedSettings ++ Seq(name := "spark-bagel")
+}
